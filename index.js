5e634fa3f2e67d42c2a793f8713ccb5417a703d1
kelvin_kiprop <kelkiprop@gmail.com>
2024-11-30 00:36:06 +0300

    arrow functions lab

---
+++ index.js
@@ -0,0 +1,9 @@
+let divide = (a, b) => a / b;
+let a = 2000;
+let b =100
+console.log(a / b);
+let square = x => x * x;
+let x = 2;
+console.log(square(2))
+let add = (a, b) => a + b;
+console.log(add(3,4))
